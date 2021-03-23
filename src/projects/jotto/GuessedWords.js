0a07995f576194afb92901dfd1695be2256b196a
Matheus Alexandre <mathe.system@gmail.com>
2021-03-22 21:31:14 -0300

    feat: ensure GuessWords renders guessed words section

--- src/projects/jotto/GuessedWords.js
+++ src/projects/jotto/GuessedWords.js
@@ -4,19 +4,43 @@ import React from 'react'
 import { testAttr } from '../../utils/add-test-att'
 
 export const GuessedWords = props => {
-  const { guessedWords } = props
+  const { guessedWords, guessedWordsRows } = props
   let contents
 
   if (guessedWords.length === 0) {
     contents = (
-      <span{...testAttr('guessed-instructions')}>
+      <span {...testAttr('guessed-instructions')}>
         Try to guess the secret word
       </span>
+    )
+  } else {
+    contents = (
+      <div {...testAttr('guessed-words')}>
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+            </tr>
+            <tr>
+              <th>Matching Letters</th>
+            </tr>
+          </thead>
+          <tbody>
+            {guessedWords.map((word, index) => (
+              <tr key={word.guessedWord} {...testAttr('guessed-word')}>
+                <td>{word.guessedWord}</td>
+                <td>{word.letterMatchCount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     )
   }
 
   return (
-    <div {...testAttr('guessed-words')}>
+    <div {...testAttr('component-guessed-words')}>
       {contents}
     </div>
   )
--- src/projects/jotto/tests/GuessedWords.test.js
+++ src/projects/jotto/tests/GuessedWords.test.js
@@ -4,12 +4,8 @@ import React from 'react'
 import { findByAttr } from '../../../text/utils/findByTestAtt'
 import { GuessedWords } from '../GuessedWords'
 
-const makeDefaultProps = () => ({
-  guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }]
-})
-
 const makeSut = (newProps = {}) => {
-  const props = { ...makeDefaultProps(), ...newProps }
+  const props = { ...fakeGuessedWords, ...newProps }
   const wrapper = shallow(<GuessedWords {...props } />)
 
   return {
@@ -17,16 +13,30 @@ const makeSut = (newProps = {}) => {
   }
 }
 
+const makeDefaultProps = () => ({
+  guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 },
+    { guessedWord: 'agile', letterMatchCount: 1 },
+    { guessedWord: 'party', letterMatchCount: 5 }]
+})
+
+const fakeGuessedWords = makeDefaultProps()
+
 describe('GuessedWords Component', () => {
   test('should render component without error', async () => {
     const { wrapper } = makeSut()
-    const sut = findByAttr(wrapper, 'guessed-words')
+    const sut = findByAttr(wrapper, 'component-guessed-words')
     expect(sut.length).toBeTruthy()
   })
 
   test('should render instructions to guess a word', async () => {
     const { wrapper } = makeSut({ guessedWords: [] })
-    const sut = findByAttr(wrapper, 'guessed-words')
+    const sut = findByAttr(wrapper, 'guessed-instructions')
     expect(sut.text().length).not.toBe(0)
+  })
+
+  test('should render guessed words section', async () => {
+    const { wrapper } = makeSut()
+    const sut = findByAttr(wrapper, 'guessed-word')
+    expect(sut.length).toBe(fakeGuessedWords.guessedWords.length)
   })
 })
