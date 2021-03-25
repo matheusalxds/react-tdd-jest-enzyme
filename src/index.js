5547d2ddbc0ca8eaf2c91f9e73467882743a9d20
Matheus Alexandre <mathe.system@gmail.com>
2021-03-24 21:07:11 -0300

    chore: add redux

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import './styles/main.scss'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 
 import App from './components/App/App'
+import store from './projects/jotto/config/configureStore'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
